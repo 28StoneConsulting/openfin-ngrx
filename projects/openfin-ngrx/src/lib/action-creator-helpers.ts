47806ee223850e91b91d5ea1dd6a3929cc75eb5a
Alexeys Valners <avalner@gmail.com>
2021-06-07 08:36:53 +0300

    feat(helpers): added routingPayloadProps helper

--- projects/openfin-ngrx/src/lib/action-creator-helpers.ts
+++ projects/openfin-ngrx/src/lib/action-creator-helpers.ts
@@ -1,6 +1,11 @@
 import { ActionReceiver } from './openfin-ngrx-metareducer.service';
 
-export function routingProps<T>(receivers: ActionReceiver | ActionReceiver[], remoteOnly = false) {
+export function routingPayloadProps<T>(receivers: ActionReceiver | ActionReceiver[], remoteOnly = false) {
   return (payload?: T) =>
     payload ? { payload, routing: { receivers, remoteOnly } } : { routing: { receivers, remoteOnly } };
+}
+
+export function routingProps<T>(receivers: ActionReceiver | ActionReceiver[], remoteOnly = false) {
+  return (props?: T) =>
+    props ? { ...props, routing: { receivers, remoteOnly } } : { routing: { receivers, remoteOnly } };
 }
